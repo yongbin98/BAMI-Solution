6e2b1dd5043f3c57086b8b4c84fb11b2f5254ed6
yb <ttong6938@khu.ac.kr>
2022-11-17 18:19:09 +0900

    test init

--- app/src/androidTest/java/com/example/patient_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/patient_app/ExampleInstrumentedTest.kt
@@ -17,6 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
+        val test:Int
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.patient_app", appContext.packageName)
