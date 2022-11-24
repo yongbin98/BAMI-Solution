fb4f7c78d3723879718600717ced1250c1108fc1
test <jina3784@gmail.com>
2022-11-24 18:07:32 +0900

    fix : Main Layer 추가

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val aa = "hello"
         setContentView(R.layout.activity_main)
     }
 }
