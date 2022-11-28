1b2d2603bcb20a622db2a65d02402a4dca909bba
test <jina3784@gmail.com>
2022-11-28 18:44:56 +0900

    로딩화면~

--- app/src/main/java/com/example/first/LoadingActivity.kt
+++ app/src/main/java/com/example/first/LoadingActivity.kt
@@ -1,11 +1,41 @@
 package com.example.first
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_loading.*
 
 class LoadingActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+        logo.animate().apply {
+        duration=1500
+        rotationYBy(180f)
+
+
+
+        }
+
+
+
+    }
+
+    companion object{
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.first
-
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val aa = "hello"
         setContentView(R.layout.activity_main)
+
+        //Weather
+
+
+        // Date
+        val utilDate = Date()
+        val formatType = SimpleDateFormat("yyyy년 MM월 dd일")
+        date.text = formatType.format(utilDate)
+
+
+        //BPM
+
+
+
+
+
     }
-}
+
+    }
